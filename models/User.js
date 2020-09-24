bb9377977f363484ad2daacf679181b0627ab58f
mather <misaka@misaka.im>
2020-09-24 17:03:53 +0800

    hashing passwords

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
+const bcrypt = require('bcrypt')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -14,6 +15,20 @@ const userSchema = new mongoose.Schema({
     required: [true, 'Please enter an password'],
     minlength: [6, 'Minimum password lenth is 6 characters']
   }
+})
+
+// fire a function after doc saved to db
+userSchema.post('save', function(doc, next){
+  console.log('new user was created & saved', doc)
+  next();
+})
+
+// fire a function before doc saved to db
+userSchema.pre('save', async function(next) {
+  const slat = await bcrypt.genSalt()
+  this.password = await bcrypt.hash(this.password, slat);
+  console.log('new user was created & saved', this)
+  next();
 })
 
 const User = mongoose.model('user', userSchema)
