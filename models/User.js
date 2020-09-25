2482ef43b5f129e8e6178cd7ba42c9c27155c2ad
mather <misaka@misaka.im>
2020-09-25 12:06:38 +0800

    Logging Users in

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const { isEmail } = require('validator')
 const bcrypt = require('bcrypt')
+const e = require('express')
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -30,6 +31,20 @@ userSchema.pre('save', async function(next) {
   console.log('new user was created & saved', this)
   next();
 })
+
+// static method to login user 
+userSchema.statics.login = async function(email, password) {
+  const user = await this.findOne({email})
+  if (user) {
+    const auth = await bcrypt.compare(password, user.password)
+    if (auth) {
+      return user
+    } else {
+      throw Error('incorrect password')
+    }
+  }
+  throw Error('incorrect email')
+}
 
 const User = mongoose.model('user', userSchema)
 
