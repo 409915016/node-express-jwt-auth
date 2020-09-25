8c2de333c312b317359a28f57624631752c24816
mather <misaka@misaka.im>
2020-09-25 11:17:27 +0800

    user signup

--- app.js
+++ app.js
@@ -27,22 +27,15 @@ app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes)
 
 //cookies
-app.get('/set-cookies', (req, res)=>{
 
-  //res.setHeader('Set-Cookie', 'newUser=true')
+// app.get('/set-cookies', (req, res)=>{
+//   res.cookie('newUser', false)
+//   res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true})
+//   res.send('you got the cookies!')
+// })
 
-  res.cookie('newUser', false)
-  res.cookie('isEmployee', true, {maxAge: 1000 * 60 * 60 * 24, httpOnly: true})
-
-  res.send('you got the cookies!')
-})
-
-
-app.get('/read-cookies', (req, res)=>{
-
-  const cookie = req.cookies;
-  console.log(cookie)
-
-  res.json(cookie)
-
-})
+// app.get('/read-cookies', (req, res)=>{
+//   const cookie = req.cookies;
+//   console.log(cookie)
+//   res.json(cookie)
+// })
--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User')
+const jwt = require('jsonwebtoken')
 
 const handleErrors = (err) => {
   console.log(err.message, err.code)
@@ -20,6 +21,14 @@ const handleErrors = (err) => {
   return errors
 }
 
+const maxAge = 3 * 24 * 60 * 60
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: maxAge
+  })
+}
+
 module.exports.signup_get = (req, res) =>{
   res.render('signup')
 }
@@ -33,7 +42,9 @@ module.exports.signup_post = async (req, res) =>{
   
   try {
     const user = await User.create({email, password})
-    res.status(201).json(user)
+    const token = createToken(user._id)
+    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000})
+    res.status(201).json({user: user._id})
   }
   catch (err) {
     const errors = handleErrors(err)
